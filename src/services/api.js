8d92b9aa4de59955a175c1d21330aba8aa1a1aad
Sameer Mahendra Kanade <110261660+MGSAMEER@users.noreply.github.com>
2025-12-31 23:21:13 +0530

    min

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,11 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 
+  process.env.REACT_APP_API_BASE_URL ||
+  process.env.VITE_APP_API_BASE_URL;
+
+console.log("API BASE URL🔥=", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
